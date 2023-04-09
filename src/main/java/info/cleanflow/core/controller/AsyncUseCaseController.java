3b00e82fa6d4e21e6c556a8a852604b860afb0da
Adrian Vega Gomez <darkcavie@gmail.com>
2023-04-09 16:57:45 +0200

    Added retries

--- src/main/java/info/cleanflow/core/controller/AsyncUseCaseController.java
+++ src/main/java/info/cleanflow/core/controller/AsyncUseCaseController.java
@@ -1,6 +1,7 @@
 package info.cleanflow.core.controller;
 
 import info.cleanflow.core.Flow;
+import org.slf4j.Logger;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -8,8 +9,13 @@ import java.util.concurrent.Future;
 import java.util.function.Consumer;
 
 import static info.cleanflow.core.Objects.nonNullArgument;
+import static org.slf4j.LoggerFactory.getLogger;
 
 public abstract class AsyncUseCaseController {
+
+    private static final Logger LOG = getLogger(AsyncUseCaseController.class);
+
+    protected static final int RETRY_TIMES = 3;
 
     private final ExecutorService executorService;
 
@@ -23,11 +29,34 @@ public abstract class AsyncUseCaseController {
         return Executors.newCachedThreadPool(threadFactory);
     }
 
-    public <S, T> Future<Void> start(final Flow<S, T> flow,
+    protected <S, T> Future<Void> start(final Flow<S, T> flow,
                                      final S value,
                                      final Consumer<T> consumer) {
         final var startFlow = new StartFlow<>(flow, value, consumer);
         return executorService.submit(startFlow, null);
+    }
+
+    protected <S, T> void retry(final Flow<S, T> flow,
+                                final S value,
+                                final Consumer<T> consumer) {
+        retry(flow, value, consumer, RETRY_TIMES);
+    }
+
+    protected <S, T> void retry(final Flow<S, T> flow,
+                                final S value,
+                                final Consumer<T> consumer,
+                                final int remindTries) {
+        try {
+            flow.flows(value, consumer);
+            LOG.info("Success reminded {} tries", remindTries);
+        } catch (RuntimeException runtimeException) {
+            if(remindTries <= 1) {
+                throw runtimeException;
+            }
+            final var moreTimes = remindTries - 1;
+            LOG.info("Try again {} times more", moreTimes);
+            retry(flow, value, consumer, moreTimes);
+        }
     }
 
 }
--- src/test/java/info/cleanflow/core/controller/AsyncUseCaseControllerTest.java
+++ src/test/java/info/cleanflow/core/controller/AsyncUseCaseControllerTest.java
@@ -6,11 +6,19 @@ import org.junit.jupiter.api.Test;
 import java.util.concurrent.Future;
 import java.util.function.Consumer;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class AsyncUseCaseControllerTest {
 
     private AsyncUseCaseControllerMock impl;
+
+    private int runCounter;
+
+    private int successCounter;
 
     @BeforeEach
     void setUp() {
@@ -27,6 +35,35 @@ class AsyncUseCaseControllerTest {
     }
 
     void flowWithoutResult(Object object, Consumer<Object> objectConsumer) {}
+
+    @Test
+    void retryOnce() {
+        impl.retry(this::flowAlwaysWorks, null, x -> successCounter++);
+        assertEquals(1, successCounter);
+        assertEquals(1, runCounter);
+    }
+
+    void flowAlwaysWorks(Void source, Consumer<Object> consumer) {
+        runCounter++;
+        consumer.accept(new Object());
+    }
+
+    @Test
+    void retryMax() {
+        final Object value;
+        final Consumer<Object> objectConsumer;
+
+        value = new Object();
+        objectConsumer = x -> fail("Must not call");
+        assertThrows(IllegalStateException.class,
+                () -> impl.retry(this::flowAlwaysFails, value, objectConsumer));
+        assertEquals(AsyncUseCaseController.RETRY_TIMES, runCounter);
+    }
+
+    void flowAlwaysFails(Object source, Consumer<Object> consumer) {
+        final var message = String.format("Fail number %d", ++runCounter);
+        throw new IllegalStateException(message);
+    }
 
     static class AsyncUseCaseControllerMock extends AsyncUseCaseController {
 
